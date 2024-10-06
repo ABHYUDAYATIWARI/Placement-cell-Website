317e07d22924c5f0c266209f9cc94321d40c2b6e
abhy <b23cs1085@iitj.ac.in>
2024-10-06 19:23:39 +0530

    fixed header and job card

--- src/components/Jobs.jsx
+++ src/components/Jobs.jsx
@@ -6,20 +6,21 @@ const JobList = ({ jobs }) => {
   const toDetails = (job) => {
     navigate(`/job-detail/${job.id}`); // Assuming each job has a unique 'id' field
   };
-
+  const logo="https://img.freepik.com/premium-vector/minimalist-logo-design-any-corporate-brand-business-company_1253202-77511.jpg"
   return (
     <div className="job-list">
       {jobs.length > 0 ? (
         jobs.map((job, idx) => (
-          <div key={idx} className="job-card">
+          <div key={idx} className="job-card flex">
+            <img src={logo} alt="" className="w-64 h-64"  />
             <button onClick={() => toDetails(job)}>
               <h2>{job.jobDesignation}</h2>
               <p><strong>Company:</strong> {job.name}</p>
               <p><strong>Sector:</strong> {job.sector.join(", ")}</p>
               <p><strong>Category:</strong> {job.category}</p>
-              <p><strong>Location:</strong> {job.jobLocation}</p>
-              <p><strong>Joining Date:</strong> {job.joiningDate}</p>
-              <p><strong>Description:</strong> {job.description}</p>
+              <p><strong>CTC:</strong> {job.recruitmentData.yearWiseRecruitmentTrends.at(-1).averageCTC}</p>
+              <p><strong>Joining Date:</strong> {job.jobDetails.tentativeJoiningDate}</p>
+              <p><strong>Location:</strong> {job.jobDetails.tentativeJobLocation}</p>
             </button>
           </div>
         ))
